6fb7c4d99a359a65d95e2c77fdd819abcbc3bcb6
Isl-n <gazi.ign@gmail.com>
2024-11-21 15:57:40 +0200

    new test project

---
+++ exe_1_QA_2024/src/main/java/exe_1_QA_2024/exe_1_QA_2024/Ex_4.java
@@ -0,0 +1,48 @@
+package exe_1_QA_2024.exe_1_QA_2024;
+
+public class Ex_4 {
+
+	public static String Compare(double a, double b, String mod) {
+		
+		switch(mod) {
+		  
+		case "regulars":
+			  
+		    if (a < b)
+		    	return "B";
+		    
+		    else if(a>b)
+		    	return "A";
+		    
+		    else
+		    	return "Error";
+		
+		  case "negatives":
+			  if(-a < -b)
+				  return "B";
+			  
+			  else if(-a > -b)
+			    	return "A";
+			
+			  else
+				  return "Error";
+		 
+		  case "reciprocals":
+			  if(1/a < 1/b)
+				  return "B";
+			  
+			  else if(1/a > 1/b)
+			    	return "A";
+			
+			  else
+				  return "Error";
+			  
+			  
+		  default:
+			  return "None";
+
+		}
+
+	}
+		
+}
